0311cd654a529fdb9b96c1065696f499755a3a1b
Álvaro Palma <palma.alvaro@outlook.com>
2022-06-07 17:24:42 -0300

    add: challenge 1021 solution

--- beginner/1021-banknotes-and-coins/index.js
+++ beginner/1021-banknotes-and-coins/index.js
@@ -3,27 +3,13 @@ var monetaryValue = parseFloat(input);
 
 const resolution = (total) => {
   const billsAndCoins = [100.00, 50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.25, 0.10, 0.05, 0.01];
-  const exchange = billsAndCoins.map(_item => 0);
-  let rest = total;
-  for (const [index, value] of billsAndCoins.entries()) {
-    while (rest >= value) {
-      exchange[index] += 1;
-      rest -= value;
-    }
-  }
+  const exchange = billsAndCoins.reduce((acc, value) => {
+    acc[value] = parseInt(total / value);
+    total = total % value + 0.00001;
+    return acc;
+  }, {});
 
-  const printResults = () => {
-    console.log(`NOTAS:`);
-    for (let index = 0; index < 6; index += 1) {
-      console.log(`${exchange[index]} nota(s) de R$ ${billsAndCoins[index].toFixed(2)}`);
-    }
-    console.log(`MOEDAS:`);
-    for (let index = 6; index < exchange.length; index += 1) {
-      console.log(`${exchange[index]} moeda(s) de R$ ${billsAndCoins[index].toFixed(2)}`);
-    }
-  }
-
-  return printResults();
+  return `NOTAS:\n${exchange['100']} nota(s) de R$ 100.00\n${exchange['50']} nota(s) de R$ 50.00\n${exchange['20']} nota(s) de R$ 20.00\n${exchange['10']} nota(s) de R$ 10.00\n${exchange['5']} nota(s) de R$ 5.00\n${exchange['2']} nota(s) de R$ 2.00\nMOEDAS:\n${exchange['1']} moeda(s) de R$ 1.00\n${exchange['0.5']} moeda(s) de R$ 0.50\n${exchange['0.25']} moeda(s) de R$ 0.25\n${exchange['0.1']} moeda(s) de R$ 0.10\n${exchange['0.05']} moeda(s) de R$ 0.05\n${exchange['0.01']} moeda(s) de R$ 0.01`;
 }
 
-resolution(monetaryValue);
+console.log(resolution(monetaryValue));
