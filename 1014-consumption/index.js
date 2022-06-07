aeaab90bf30ba836228a8bdc7518038cfa63152b
Álvaro Palma <palma.alvaro@outlook.com>
2022-06-07 14:09:46 -0300

    fix: begginer folder

--- 1014-consumption/index.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-const resolution = (lines) => {
-  const consumption = parseInt(lines[0]) / parseFloat(lines[1]);
-  return `${consumption.toFixed(3)} km/l`
-};
-
-console.log(resolution(lines));
--- 1016-distance/index.js
+++
@@ -1,3 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-
-console.log(`${parseInt(input) * 2} minutos`);
