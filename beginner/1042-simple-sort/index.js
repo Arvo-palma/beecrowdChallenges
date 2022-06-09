02f0f885dded6c5928884bd4366c68d9f8271332
Álvaro Palma <palma.alvaro@outlook.com>
2022-06-09 15:23:49 -0300

    add: challenges 1042, 1043 and 1044

--- beginner/1042-simple-sort/index.js
+++ beginner/1042-simple-sort/index.js
@@ -1,2 +1,27 @@
 const filePath = require("path").resolve(__dirname, "./dev/stdin");
 const input = require("fs").readFileSync(filePath, "utf8");
+
+// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+const values = lines[0].split(' ').map(value => parseInt(value));
+
+const resolution = (values) => {
+  const workArray = values;
+  workArray.sort((a,b) => {
+    return a - b;
+  });
+  return workArray;
+}
+
+const formatAnswer = (values) => {
+  const orderedArray = resolution(values);
+  orderedArray.forEach(value => {
+    console.log(value);
+  });
+  console.log();
+  lines[0].split(' ').forEach(value => {
+    console.log(parseInt(value));
+  });
+}
+
+formatAnswer(values);
--- beginner/1043-triangle/index.js
+++ beginner/1043-triangle/index.js
@@ -1,2 +1,18 @@
 const filePath = require("path").resolve(__dirname, "./dev/stdin");
 const input = require("fs").readFileSync(filePath, "utf8");
+
+// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+const values = lines[0].split(' ').map(value => parseFloat(value));
+
+const isTriangle = () => {
+  const [b1, b2, h] = values;
+  const [A, B, C] = values.sort((a,b) => {
+    return a - b;
+  });
+  const exists = (A + B) > C;
+  return exists ? `Perimetro = ${(A+B+C).toFixed(1)}` : `Area = ${(((b1 + b2)*h)/2).toFixed(1)}`;
+};
+
+
+console.log(isTriangle());
