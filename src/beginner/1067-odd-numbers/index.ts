3ab27b5d7f89ff152722a0bd13ce07775262a5ca
Álvaro Palma <palma.alvaro@outlook.com>
2022-06-28 10:42:16 -0300

    add: solve challenges up to 1073

--- src/beginner/1067-odd-numbers/index.ts
+++ src/beginner/1067-odd-numbers/index.ts
@@ -1,2 +1,19 @@
 const filePath = require("path").resolve(__dirname, "./dev/stdin");
 const input = require("fs").readFileSync(filePath, "utf8");
+
+// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+
+const prepareInput = (input: string) => {
+  const lines = input.split("\n");
+  const value = parseInt(lines[0]);
+  return value;
+};
+
+export const resolution = (input: string) => {
+  const value = prepareInput(input);
+  for (let num = 1; num <= value; num += 2) {
+    console.log(num);
+  }
+};
+
+resolution(input);
